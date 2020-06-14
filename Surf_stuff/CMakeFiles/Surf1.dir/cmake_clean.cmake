920ab219766b7241ffce7918445cac8e8066d40d
Ramdas Tiwari <rtiwari@iitk.ac.in>
2020-06-14 23:49:22 +0530

    Surf Script matches features in two pictures

---
+++ Surf_stuff/CMakeFiles/Surf1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Surf1.dir/Surf1.cpp.o"
+  "Surf1.pdb"
+  "Surf1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Surf1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
