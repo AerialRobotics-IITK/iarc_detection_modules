7eea4e879d131484087329f0872e8bb87cd35fa4
Ramdas Tiwari <rtiwari@iitk.ac.in>
2020-07-01 00:49:30 +0530

    This node should work. I am working on cleaning it up by writing a kfast_iarc.cpp file which will store all the functions etc by using namespace.

--- kfast_iarc/src/kfast_iarc_node.cpp
+++ kfast_iarc/src/kfast_iarc_node.cpp
@@ -6,18 +6,21 @@ ros::Publisher grayscale_image;
 ros::Publisher keypoint_vectors;
 
 std::vector<Keypoint> KFAST_kps;
+cv_bridge::CvImage img_bridge;
+std_msgs::Int32MultiArray kfast_keypoints;
 
 
 void CallBack(const sensor_msgs::ImageConstPtr &msg){
-  // constexpr bool display_image = true;
-	constexpr bool nonmax_suppress = true;
-	constexpr auto warmups = 150;
-	constexpr auto runs = 1000;
-	constexpr auto thresh = 50;
-	constexpr bool KFAST_multithread = true;
-	// constexpr char name[] = "test.jpg";
+    constexpr bool nonmax_suppress = true;
+    constexpr auto warmups = 150;
+    constexpr auto runs = 1000;
+    constexpr auto thresh = 50;
+    constexpr bool KFAST_multithread = true;
+    // constexpr char name[] = "test.jpg";
     
-    int counter = 0;
+    // grayscale_image =  nh.advertise<sensor_msgs::Image>("kfast_grayscale", 1);    
+    // keypoint_vectors = nh.advertise<std_msgs::Int32MultiArray>("kfast_keypoints", 1);
+
     cv_bridge::CvImagePtr cv_ptr;
     try {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -26,53 +29,51 @@ void CallBack(const sensor_msgs::ImageConstPtr &msg){
     }
     cv::Mat frame, image, image_gray;
     frame = cv_ptr->image;
-    // ROS_ASSERT(frame.empty() != true);
     cv::cvtColor(frame, image_gray, CV_BGR2GRAY);
-    // if (!image.data) {
-    //     std::cerr << "ERROR: failed to open image. Aborting." << std::endl;
-    //     return;
-    // }
-    cv_bridge::CvImage img_bridge;
-    img_bridge.encoding = sensor_msgs::image_encodings::MONO8;
+    // cv_bridge::CvImage img_bridge;
     img_bridge.header.stamp = ros::Time::now();
     img_bridge.image = image_gray;
-    grayscale_image.publish(img_bridge.toImageMsg());
+    // grayscale_image.publish(img_bridge.toImageMsg());
 
     for (int i = 0; i < warmups; ++i) KFAST<KFAST_multithread, nonmax_suppress>(image.data, image.cols, image.rows, static_cast<int>(image.step), KFAST_kps, thresh);
 	{
-		// const high_resolution_clock::time_point start = high_resolution_clock::now();
 		for (int32_t i = 0; i < runs; ++i) {
 			KFAST<KFAST_multithread, nonmax_suppress>(image.data, image.cols, image.rows, static_cast<int>(image.step), KFAST_kps, thresh);
 		}
-		// const high_resolution_clock::time_point end = high_resolution_clock::now();
 	}
-    std_msgs::Int32MultiArray kfast_keypoints;
-    // int arr[3] = {KFAST_kps.x, KFAST_kps.y, KFAST_kps.score};
-    // int a = KFAST_kps.x;
+    std::vector<int>vec(3*KFAST_kps.size(), 0);
+    for (uint i=0; i<KFAST_kps.size(); i++){
+      vec[i] = KFAST_kps[i].x;
+      vec[i+1] = KFAST_kps[i].y;
+      vec[i+2] = KFAST_kps[i].score;
+    }
+
+    // std_msgs::Int32MultiArray kfast_keypoints;
+
     kfast_keypoints.layout.dim.push_back(std_msgs::MultiArrayDimension());
-    kfast_keypoints.layout.dim[0].size = KFAST_kps.size();
-    kfast_keypoints.layout.dim[0].stride = 1;
-
-    kfast_keypoints.data.clear();
-    kfast_keypoints.data.insert(kfast_keypoints.data.end(), KFAST_kps.begin(), KFAST_kps.end());
-    keypoint_vectors.publish(KFAST_kps);
-
-    keypoint_vectors.publish(kfast_keypoints);
+    kfast_keypoints.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    kfast_keypoints.layout.dim[0].size = 3;
+    kfast_keypoints.layout.dim[1].size = KFAST_kps.size();
+    kfast_keypoints.layout.dim[0].stride = 3*KFAST_kps.size();
+    kfast_keypoints.layout.dim[1].stride = 3;
+    kfast_keypoints.data = vec;
+    // keypoint_vectors.publish(kfast_keypoints);
     return;
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "KFAST_node");
-    // ros::NodeHandle nh;
     ros::Rate loopRate(10);
 
-    input_image = nh.subscribe("/firefly/camera_front/image_raw", 1, CallBack);
     grayscale_image =  nh.advertise<sensor_msgs::Image>("kfast_grayscale", 1);    
     keypoint_vectors = nh.advertise<std_msgs::Int32MultiArray>("kfast_keypoints", 1);
+    input_image = nh.subscribe("/firefly/camera_front/image_raw", 1, CallBack);
+
     while (ros::ok()) {
+        keypoint_vectors.publish(kfast_keypoints);
+        grayscale_image.publish(img_bridge.toImageMsg());
         ros::spinOnce();
         loopRate.sleep();
     }
     return 0;
-
 }
