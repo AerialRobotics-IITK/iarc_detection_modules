e9d2678acd290f0e5e7893cfa15a4fa0d76c93d2
Ashwin Shenai <ashwins@iitk.ac.in>
2020-07-04 22:46:38 +0530

    Separate pose estimation package
    Relocate headers and source files
    Separate params and launch file

---
+++ pose_estimator/include/pose_estimator/libpose_estimation.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <nav_msgs/Odometry.h>
+#include <tf/tf.h>
+
+namespace iarc2020::pose_estimation {
+
+class PoseEstimator {
+  public:
+    void init();
+
+    void getDistance(const float& dist);
+    Eigen::Vector3d getGlobCoord() {
+        return glob_coord_;
+    };
+
+    void setCamToQuadMatrix(const std::vector<double>& mat) {
+        cam_to_quad_ = Eigen::Matrix3d(mat.data()).transpose();
+    };
+    void setCamMatrix(const std::vector<double>& mat) {
+        cam_matrix_ = Eigen::Matrix3d(mat.data()).transpose();
+    };
+    void setTCamMatrix(const std::vector<double>& mat) {
+        t_cam_ = Eigen::Vector3d(mat.data());
+    };
+    void setVerbosity(const bool& flag) {
+        verbose_ = flag;
+    }
+
+    void setImgVec(const float& x, const float& y);
+    void setQuaternion(const nav_msgs::Odometry& odom);
+
+    void CamToQuad();
+    void QuadToGlob(const nav_msgs::Odometry& odom);
+
+  private:
+    Eigen::Matrix3d scale_up_;
+    Eigen::Matrix3d cam_matrix_;
+    Eigen::Matrix3d cam_to_quad_;
+    Eigen::Matrix3d quad_to_glob_;
+
+    Eigen::Vector3d img_vec_;
+    Eigen::Vector3d t_cam_;
+    Eigen::Vector3d quad_coord_;
+    Eigen::Vector3d glob_coord_;
+
+    bool verbose_;
+};
+
+}  // namespace iarc2020::pose_estimation
