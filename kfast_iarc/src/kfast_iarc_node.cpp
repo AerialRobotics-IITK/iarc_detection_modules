994244b1a85d1e52f926ee18b10bf67281e5d1ef
Ramdas Tiwari <rtiwari@iitk.ac.in>
2020-07-26 01:15:50 +0530

    KFAST feature detection

--- kfast_iarc/src/kfast_iarc_node.cpp
+++ kfast_iarc/src/kfast_iarc_node.cpp
@@ -2,21 +2,21 @@
 
 using namespace ariitk::kfast;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "KFAST_node");
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_private("~");
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "KFAST_node");
+    ros::NodeHandle nh;
+    ros::NodeHandle nh_private("~");
 
-  kfast kf;
+    kfast kf;
 
-  kf.init(nh, nh_private);
+    kf.init(nh, nh_private);
 
-  ros::Rate loopRate(10);
+    ros::Rate loopRate(10);
 
-  while (ros::ok()) {
-    ros::spinOnce();
-    kf.run();
-    loopRate.sleep();
-  }
-  return 0;
+    while (ros::ok()) {
+        ros::spinOnce();
+        kf.run();
+        loopRate.sleep();
+    }
+    return 0;
 }
